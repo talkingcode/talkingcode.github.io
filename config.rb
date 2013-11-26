a2273b9cf1ee45065bcfb64e6a05bf432bf19400
Matt Goldman <matt@smallhq.com>
2013-11-25 21:22:42 -0800

    initial commit

---
+++ config.rb
@@ -0,0 +1,73 @@
+Time.zone = "Pacific Time (US & Canada)"
+
+###
+# Compass
+###
+
+# Change Compass configuration
+compass_config do |config|
+  config.output_style = :compact
+end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+###
+# Helpers
+###
+activate :livereload
+
+activate :syntax
+
+activate :blog do |blog|
+  blog.permalink = ":category/:title.html"
+  blog.layout = "blog_layout"
+  blog.prefix = "blog"
+end
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+
+  deploy.method = :git
+  deploy.remote = "github"
+  deploy.branch = "gh-pages"
+end
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+set :markdown_engine, :redcarpet
+set :markdown, fenced_code_blocks: true, smartypants: true
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  # Enable cache buster
+  activate :asset_hash
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
