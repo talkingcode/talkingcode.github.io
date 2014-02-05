354067761a48dfcc4003d61a0c3d226081bb036b
Joelle Steiniger <joelle@smallhq.com>
2014-02-04 20:55:41 -0800

    fixed header image and added animation. Added promo to homepage

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,1 +1,13 @@
 //= require_tree .
+
+$(document).ready(function(){
+  setTimeout(function(){
+    $('#left_header').fadeIn(700, function(){
+      $('#right_header').animate({paddingTop: '16px'}, 1000, 'easeOutQuint');
+    });
+  }, 250);
+
+  setTimeout(function(){
+    $("#hello-there").animate({top: '0px'}, 500, 'easeOutBounce');
+  }, 1000);
+});
