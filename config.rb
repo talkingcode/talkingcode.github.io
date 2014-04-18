5e3cab3e20b80e5cc9aa41056c9c73de9f3b58c0
Joelle Steiniger <joelle@smallhq.com>
2014-04-18 09:31:43 -0700

    created first batch of twitter cards

--- config.rb
+++ config.rb
@@ -73,7 +73,7 @@ configure :build do
   activate :minify_javascript
 
   # Enable cache buster
-  activate :asset_hash
+  activate :asset_hash, ignore: [/^images\/twitter_cards/]
 
   # Use relative URLs
   # activate :relative_assets
